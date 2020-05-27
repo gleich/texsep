0d32db7d500df5d97d82d3b332127261ac478791
Matthew Gleich <matthewgleich@gmail.com>
2020-05-27 13:42:33 -0400

    ✨ Add ascii title
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -10,29 +10,27 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Use:   "texsep",
-	Short: "CLI to separate your pdfs from your tex files",
+	Use: "texsep",
+
+	Long: `
+Separate your pdfs from your tex files
+	            ___           ___            ___           ___           ___
+                   /\__\         /|  |          /\__\         /\__\         /\  \
+      ___         /:/ _/_       |:|  |         /:/ _/_       /:/ _/_       /::\  \
+     /\__\       /:/ /\__\      |:|  |        /:/ /\  \     /:/ /\__\     /:/\:\__\
+    /:/  /      /:/ /:/ _/_   __|:|__|       /:/ /::\  \   /:/ /:/ _/_   /:/ /:/  /
+   /:/__/      /:/_/:/ /\__\ /::::\__\_____ /:/_/:/\:\__\ /:/_/:/ /\__\ /:/_/:/  /
+  /::\  \      \:\/:/ /:/  / ~~~~\::::/___/ \:\/:/ /:/  / \:\/:/ /:/  / \:\/:/  /
+ /:/\:\  \      \::/_/:/  /      |:|~~|      \::/ /:/  /   \::/_/:/  /   \::/__/
+ \/__\:\  \      \:\/:/  /       |:|  |       \/_/:/  /     \:\/:/  /     \:\  \
+      \:\__\      \::/  /        |:|__|         /:/  /       \::/  /       \:\__\
+       \/__/       \/__/         |/__/          \/__/         \/__/         \/__/
+	`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	cobra.OnInitialize()
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.texsep.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
