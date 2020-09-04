e92d698ce2ea9a5edb06fceb3015a6baacb2221e
Matthew Gleich <matthewgleich@gmail.com>
2020-09-04 18:36:53 -0400

    🔥 Remove folders cmd for now
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- cmd/folders.go
+++ cmd/folders.go
@@ -1,19 +1,14 @@
 package cmd
 
-import (
-	"github.com/Matt-Gleich/statuser/v2"
-	"github.com/spf13/cobra"
-)
-
 // foldersCmd represents the folders command
-var foldersCmd = &cobra.Command{
-	Use:   "folders",
-	Short: "Move pdf and tex files into their own folders at the current level\n\t\tSee https://bit.ly/2XHmg5Q for more info",
-	Run: func(cmd *cobra.Command, args []string) {
-		statuser.ErrorMsg("This command is not yet implemented", 1)
-	},
-}
+// var foldersCmd = &cobra.Command{
+// 	Use:   "folders",
+// 	Short: "Move pdf and tex files into their own folders at the current level\n\t\tSee https://bit.ly/2XHmg5Q for more info",
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		statuser.ErrorMsg("This command is not yet implemented", 1)
+// 	},
+// }
 
 func init() {
-	rootCmd.AddCommand(foldersCmd)
+	// rootCmd.AddCommand(foldersCmd)
 }
